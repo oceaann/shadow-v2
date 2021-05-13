8497beeb3205e3d0879aea2ce566f262b5a6f283
oceaann <kontakt.ocaen@gmail.com>
2021-05-13 18:38:20 +0200

    Globalchat message structure

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 const { config } = require("dotenv")
 config()
 module.exports = {
+    inviteContent: "Jestem idiotą i staram się wysłac zaproszenie :)",
     TOKEN: process.env.TOKEN
 }
---
+++ src/structures/message.js
@@ -0,0 +1,37 @@
+const inviteRegEx = new RegExp(/(https?:\/\/)?(www.)?(discord.(gg|io|me)|(discordapp|discord).com\/invite)\/[^\s\\/]+?(?=\b)/gmi)
+const { inviteContent } = require("../config/config")
+
+module.exports = class {
+
+    constructor(data, id) {
+
+        this.content = data.content || null
+        
+        if(this.content && inviteRegEx.test(this.content)) {
+            this.content = inviteContent
+        } else if(data.activity) {
+            if(data.activity.party_id.startsWith("spotify:")) this.content = `[Spotify invite: ${msg.activity.party_id}]`
+            else this.content = "[Game invite]"
+        }
+
+        this.attachments = (data.attachments || []).map(a => a.url)
+
+        if(data.stickers) {
+            this.content = `${this.content || ""}\n[Sticker]`
+        }
+
+        if(data.embeds && data.embeds[0]) {
+            if(data.author.bot) {
+                this.embeds = data.embeds
+            } else {
+                this.content = "[Embed]"
+            }
+        }
+
+        this.name = `${data.author.username}#${data.author.discriminator} ${id}`
+        this["avatar_url"] = `https://cdn.discordapp.com/avatars/${data.author.id}/${data.author.avatar}?size=256`
+        this["allowed_mentions"] = { "parse": [] }
+
+    }
+
+}
