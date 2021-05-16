fcd6234ea8938001762c29480780201ee48ff92f
oceaann <kontakt.ocaen@gmail.com>
2021-05-16 15:54:47 +0200

    Websocket sender support

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ client.gchannels = []
 client.ids = {}
 client.mutes = []
 client.webhooks = {}
+client.wsClients = []
 
 require("./handlers/modules")
 require("./handlers/commands")
@@ -17,4 +18,7 @@ if(senderMode === 1) {
     client.sender = require("./senders/single")
 } else if(senderMode === 2) {
     client.sender = require("./senders/fetch")
+} else if(senderMode === 3) {
+    require("./server")
+    client.sender = require("./senders/ws")
 }
