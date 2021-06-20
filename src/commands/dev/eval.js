62cf7fc0bdb3d54a3bcba63e3ddc1ad0975555de
oceaann <kontakt.ocaen@gmail.com>
2021-06-20 17:50:48 +0200

    eval command only enable in debug mode

--- src/commands/dev/eval.js
+++ src/commands/dev/eval.js
@@ -1,3 +1,5 @@
+const client = require("../../index")
+
 module.exports = class {
 
     constructor() {
@@ -6,15 +8,17 @@ module.exports = class {
     }
 
     async run(msg, args) {
-        
+
+        if(!client.debug) return "Komenda eval jest dostępna jedynie w trybie debugowania"
+
         const code = args.join(" ")
 
         try {
             
             let evaled = await eval(code)
-        
+
             if (typeof evaled !== "string") evaled = require("util").inspect(evaled)
-        
+
             return ({ embed: { description: `\`\`\`${evaled}\`\`\`` } })
 
         } catch (e) {
